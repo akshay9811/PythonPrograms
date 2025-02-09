e1a8d79359d6760cce09bf86963010387f4e3898
akshay9811 <akshay.apsdk@gmail.com>
2025-02-09 12:35:48 +0530

    Created Calculator Functions

---
+++ Functions/calculator.py
@@ -0,0 +1,17 @@
+num1 = 20
+num2 = 5
+def add(num1, num2):
+    c = num1 + num2
+    print(c)
+
+def sub(num1, num2):
+    c = num1 - num2
+    print(c)
+
+def mul(num1, num2):
+    c = num1 * num2
+    print(c)
+
+add(num1, num2)
+sub(num1, num2)
+mul(num1, num2)
