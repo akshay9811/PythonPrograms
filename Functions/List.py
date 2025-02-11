bcf1a2573219c5559b49c92618fe88eb418452d1
akshay9811 <akshay.apsdk@gmail.com>
2025-02-11 12:45:16 +0530

    Created list

---
+++ Functions/List.py
@@ -0,0 +1,9 @@
+student_names = ["Akshay", "Rahul", "Ram", "Raja"]
+print(student_names)
+student_names.append("Rani")
+print(student_names)
+student_names.remove("Raja")
+print(student_names)
+admins = ("Akshay", "Ram")
+print(len(admins))
+print(len(student_names))
